697fcba55f8ed2bc582bd8ae5983c41e001e48f5
rikhil.tanugula@gmail.com <rikhil.tanugula@gmail.com>
2026-03-05 10:58:57 -0800

    added text field

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,9 +13,15 @@ public class Main extends Application {
     public void start(Stage stage) {
         Label label = new Label("Hello JavaFX (Gradle)");
         Button button = new Button("Click me");
+        TextField textField = new TextField(); 
         button.setOnAction(e -> label.setText("Clicked!"));
+        
+        button.setDisable(true);
+        textField.textProperty().addListener((observable, oldValue, newValue) ->
+            button.setDisable(newValue.isEmpty())
+        );
 
-        VBox root = new VBox(12, label, button);
+        VBox root = new VBox(12, label, textField, button);
         root.setStyle("-fx-padding: 18; -fx-alignment: center;");
 
         stage.setTitle("HelloFXGradle");
